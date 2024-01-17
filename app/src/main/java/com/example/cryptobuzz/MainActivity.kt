005e88b2e1376895509d6c41662dd96d414f10b3
Harsh Vardhan Tewari <harshv12.hv29@gmail.com>
2024-01-17 10:16:54 +0530

    removed action bar

--- app/src/main/java/com/example/cryptobuzz/MainActivity.kt
+++ app/src/main/java/com/example/cryptobuzz/MainActivity.kt
@@ -26,7 +26,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (actionBar != null) {
+            actionBar?.hide();
+        }
         setContent {
             CryptoBuzzTheme {
                 // A surface container using the 'background' color from the theme
